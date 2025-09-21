fd3dc1d140900b200cfac507ba419813efbddc56
Kostino_DV <130689524+Pionabu@users.noreply.github.com>
2025-09-21 23:02:39 +0200

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,11 @@
-shared_script '@KostinoPub/shared_fg-obfuscated.lua'
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Pio'
-description 'Visualizza gli ID'
+author 'Kostino'
+description 'ShowID'
 version '1.0.0'
 
 client_scripts {
     'client.lua'
 }
 
-server_scripts {
-	--[[server.lua]]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            'temp/.specHelper.js',
-}
